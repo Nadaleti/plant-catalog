574e3d013b629cacbe45290be267ed5985b9ab9f
Nadaleti <mat.nadaleti@hotmail.com>
2020-03-23 21:47:45 -0300

    Adding some guide content to App component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import classes from './App.module.css';
 function App() {
   return (
     <div className={classes.App}>
+      <div>Toolbar</div>
+      <div>Plants catalog</div>
     </div>
   );
 }
