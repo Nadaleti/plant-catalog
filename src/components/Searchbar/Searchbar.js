ddb718178be3ef4df4bcff4abe406518f7644fb9
Nadaleti <mat.nadaleti@hotmail.com>
2020-03-24 14:37:06 -0300

    PC-2 Adding new style to searchbar and toolbar component

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -7,12 +7,15 @@ import classes from './Searchbar.module.scss';
 
 const searchbar = (props) => {
   return (
-    <div className={classes.Searchbar}>
+    <form className={classes.Searchbar}>
       <input type="text"
         onFocus={props.focused} onBlur={props.blur}
         placeholder="Plant common or scientific name..." />
-      <FontAwesomeIcon icon={faSearch} className={classes.SearchIcon} />
-    </div>
+      
+      <button className={classes.SearchButton} type="submit">
+        <FontAwesomeIcon icon={faSearch} className={classes.SearchIcon} />
+      </button>
+    </form>
   )
 }
 
